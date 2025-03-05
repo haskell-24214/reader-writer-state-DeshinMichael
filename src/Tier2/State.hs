d9e0253d98ed22656da310d38c84a8d56f0f990c
DeshinMichael <deshinmisha26@mail.ru>
2025-03-05 22:43:23 +0700

    updated State.hs в Tier2

--- src/Tier2/State.hs
+++ src/Tier2/State.hs
@@ -9,28 +9,76 @@ emptyRegisters = Registers 0 0 False 0
 type Calculation = State Registers Int
 
 plus :: Calculation
-plus = undefined
+plus = do
+  reg <- get
+  let a = ax reg
+      b = bx reg
+      r = a + b
+  put reg { acc = r, blink = False }
+  return r
 
 minus :: Calculation
-minus = undefined
+minus = do
+  reg <- get
+  let a = ax reg
+      b = bx reg
+      r = a - b
+  put reg { acc = r, blink = False }
+  return r
 
 productS :: Calculation
-productS = undefined
+productS = do
+  reg <- get
+  let a = ax reg
+      b = bx reg
+      r = a * b
+  put reg { acc = r, blink = False }
+  return r
 
-div :: Calculation
-div = undefined
+divS :: Calculation
+divS = do
+  reg <- get
+  let a = ax reg
+      b = bx reg
+  if b == 0
+    then do
+      put (Registers 0 0 False 0)
+      return 0
+    else do
+      let r = a `div` b
+      put reg { acc = r, blink = False }
+      return r
 
 swap :: Calculation
-swap = undefined
+swap = do
+  reg <- get
+  let a = ax reg
+      b = bx reg
+  put reg { ax = b, bx = a }
+  return (acc reg)
 
 blinkS :: Calculation
-blinkS = undefined
+blinkS = do
+  reg <- get
+  let bl = blink reg
+  put reg { blink = not bl }
+  return (acc reg)
 
 accS :: Calculation
-accS = undefined
+accS = do
+  reg <- get
+  if blink reg
+    then put reg { bx = acc reg, blink = not (blink reg) }
+    else put reg { ax = acc reg, blink = not (blink reg) }
+  gets acc
 
 number :: Int -> Calculation
-number x = undefined
+number x = do
+  reg <- get
+  if blink reg
+    then put reg { bx = x, blink = not (blink reg) }
+    else put reg { ax = x, blink = not (blink reg) }
+  return (acc reg)
 
 commandToCalculation :: String -> Calculation
 commandToCalculation s =
@@ -38,6 +86,7 @@ commandToCalculation s =
     "+" -> plus
     "-" -> minus
     "*" -> productS
+    "/" -> divS
     "swap" -> swap
     "blink" -> blinkS
     "acc" -> accS
